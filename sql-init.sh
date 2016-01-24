5ec04b5d6880a673e951c96648d3f6a15219682b
slofurno <slofurno@gmail.com>
2016-01-24 15:13:25 -0500

    update sql init

--- sql-init.sh
+++ sql-init.sh
@@ -7,6 +7,7 @@ CREATE INDEX user_index on users (id);
 CREATE INDEX users_state on users (state);
 CREATE INDEX print_ad_user on prints (AdId, UserId);
 CREATE TABLE ads (id varchar(40), AccountId varchar(40), name varchar(100), path varchar(140));
-CREATE TABLE accounts (id varchar(40), email varchar(90) unique, password varchar(50), salt varchar(50));
-CREATE TABLE logins (AccountId varchar(40), token varchar(40), address varchar(40));"
+CREATE TABLE accounts (id varchar(40), email varchar(90) unique, password varchar(50), salt varchar(50), role varchar(20));
+CREATE TABLE logins (AccountId varchar(40), token varchar(40), address varchar(40));
+CREATE TABLE profiles (AccountId varchar(40), json text);"
 
